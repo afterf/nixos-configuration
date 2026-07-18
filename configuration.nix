2c9175c0b96f9dedd15a0473a92e136926a73599
afterf <acironis@gmail.com>
2026-07-18 21:51:42 +0100

    Add files via upload

--- configuration.nix
+++ configuration.nix
@@ -141,10 +141,14 @@ nixpkgs.config.permittedInsecurePackages = [
   environment.systemPackages = with pkgs; [
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   chessx
+  gping
+  mtr
   obsidian
   wget
   openjdk
   steam
+  clac
+  gparted
   steamcmd
   lm_sensors
   sublime4
